743804399f26b879035a1d3900836a81fe81c18a
“link1134” <23130099@st.hcmuaf.edu.vn>
2025-12-24 22:03:36 +0700

    Thêm filter

---
+++ DocTruyen/src/main/java/filter/AuthFilter.java
@@ -0,0 +1,61 @@
+package filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.bean.User;
+
+/**
+ * Servlet implementation class AuthFilter
+ */
+@WebFilter("/*")
+public class AuthFilter implements Filter {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	@Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+
+		String path = req.getRequestURI().substring(req.getContextPath().length());
+		if (path.startsWith("/login") || path.startsWith("/register")) {
+			chain.doFilter(request, response);
+			return;
+		}
+		HttpSession session = req.getSession(false);
+		if (session != null && session.getAttribute("user") != null) {
+			User user = (User) session.getAttribute("user");
+            if (path.startsWith("/admin") && !"ADMIN".equals(user.getRole())) {
+                res.sendRedirect(req.getContextPath() + "/login");
+                return;
+            }
+
+			chain.doFilter(request, response);
+		} else {
+			res.sendRedirect(req.getContextPath() + "/login");
+		}
+	}
+
+	public void destroy() {
+	}
+}
